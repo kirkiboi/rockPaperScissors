b79e1d03e90e911002572d0d28035db0539b8ff2
RZ_cola <168092735+kirkiboi@users.noreply.github.com>
2025-06-26 08:51:17 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,9 +6,13 @@ const AIScore = document.querySelector(".AI-score");
 const winnerPrompt = document.querySelector(".winner");
 const leftButton = document.querySelector(".left-button");
 const rightButton = document.querySelector(".right-button");
+const promptDiv = document.getElementById("prompt");
+const AIPrompt = document.getElementById("AIPrompt");
+
 let userChoice = 2;
 let userScoreCounter = 0;
 let aiScoreCounter = 0;
+
 function imagePicker(){
     if (userChoice === 1){
         userPhoto.style.backgroundImage = "url(images/rock.png)";
@@ -17,7 +21,7 @@ function imagePicker(){
     }if (userChoice === 3){
         userPhoto.style.backgroundImage = "url(images/scissors.png)";
     }
-}
+};
 leftButton.addEventListener("click", ()=>{
     if (userChoice > 1) { 
         userChoice--;
@@ -34,26 +38,36 @@ playButton.addEventListener("click", ()=>{
     let AINumber = Math.floor(Math.random() * 3 + 1);
     if (AINumber === 1){
         AIPhoto.style.backgroundImage = "url(images/rock.png)";
+        AIPrompt.innerHTML = "AI chose Rock"
     }if (AINumber === 2){
         AIPhoto.style.backgroundImage = "url(images/paper.png)";
+        AIPrompt.textContent = "AI chose Paper"
     }if (AINumber === 3){
         AIPhoto.style.backgroundImage = "url(images/scissors.png)";
+        AIPrompt.textContent = "AI chose Scissor"
     }
-    if(userChoice === 1 & AINumber === 1 || userChoice === 2 & AINumber === 2 || userChoice === 3 & AINumber === 3){
+    if(userChoice === 1 && AINumber === 1 || userChoice === 2 && AINumber === 2 || userChoice === 3 && AINumber === 3){
         winnerPrompt.textContent = "Draw";
     } 
-    if(userChoice === 1 & AINumber === 2 || userChoice === 2 & AINumber === 3 || userChoice === 3 & AINumber === 1){
+    if(userChoice === 1 && AINumber === 2 || userChoice === 2 && AINumber === 3 || userChoice === 3 && AINumber === 1){
         winnerPrompt.textContent = "You Lose!";
         userScoreCounter = Math.max(0, userScoreCounter - 1);
         aiScoreCounter++;
     }
-    if(userChoice === 1 & AINumber === 3 || userChoice === 2 & AINumber === 1 || userChoice  === 3 & AINumber === 2){
+    if(userChoice === 1 && AINumber === 3 || userChoice === 2 && AINumber === 1 || userChoice  === 3 && AINumber === 2){
         winnerPrompt.textContent = "You Won!";
         userScoreCounter++;
         aiScoreCounter = Math.max(0, aiScoreCounter - 1);
     }
     userScore.textContent = `Your Score: ${userScoreCounter}`;
     AIScore.textContent = `AI's  Score: ${aiScoreCounter}`;
+    if (userChoice === 1){
+        promptDiv.textContent = "You chose Rock /"
+    }if (userChoice === 2){
+        promptDiv.textContent = "You chose Paper / "
+    }if (userChoice === 3){
+        promptDiv.textContent = "You chose Scissors / "
+    }
 });
 document.addEventListener('keydown', function(event){
     if(event.key === "ArrowDown"){
